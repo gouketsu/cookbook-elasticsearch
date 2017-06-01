542616df18cfc43fc504f08c83e134bd0e3b0e59
Jean-Bernard Damiano <jbdamiano@gmail.com>
2017-06-01 12:28:13 +0200

    fix warning

--- attributes/proxy.rb
+++ attributes/proxy.rb
@@ -1,5 +1,5 @@
 include_attribute 'elasticsearch::default'
-include_attribute 'elasticsearch:nginx'
+include_attribute 'elasticsearch::nginx'
 
 # Try to load data bag item 'elasticsearch/aws' ------------------
 #
